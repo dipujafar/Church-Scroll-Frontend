d8209c52bb6dce8c0ee16fa845488541002071fe
dipujafar <dipujafarmr@gmail.com.>
2024-11-27 12:05:37 +0600

    added sponsor church page

---
+++ src/app/user/sponsor-church/page.tsx
@@ -0,0 +1,7 @@
+import SponsorChurchContainer from "./_components/SponsorChurchContainer";
+
+const SponsorChurchPage = () => {
+  return <SponsorChurchContainer></SponsorChurchContainer>;
+};
+
+export default SponsorChurchPage;
